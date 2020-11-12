4a8c9a09311c188781873cf5eba6e6bf33247b4f
raquel202 <moreira_raquel2020@outlook.com>
2020-11-12 14:11:58 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+
+  if(document.getElementById("nome").value != "" &&  
+      document.getElementById("email").value != "" && document.getElementById("telefone").value != ""){
+
+      alert("Pronto! Você receberá novidades por email.")
+      
+  }else{
+      alert("Por favor, preencha todos os campos!")
+  }
+}
